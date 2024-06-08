5a761045455dd837df8ce0f58c86ef86d8364678
frodriguezcode88@gmail.com <frgithub88@gmail.com>
2024-06-08 11:00:49 -0400

    Cambios-progress-indicator

--- src/app/theme/shared/components/progress-indicator/progress-indicator.component.ts
+++ src/app/theme/shared/components/progress-indicator/progress-indicator.component.ts
@@ -1,41 +1,51 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Input, OnChanges, Renderer2, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-progress-indicator',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  template: `    <div class="progress-container">
-  <div class="progress-circle">
-    <div class="progress-circle-inner">
-      <span>{{ progress }}%</span>
+  imports: [CommonModule],
+  template: ` <div class="progress-container">
+    <div class="progress-circle animate__animated" [style.background]="conicGradient">
+      <div class="progress-circle-inner">
+        <span>{{ progress }}%</span>
+      </div>
     </div>
-  </div>
-</div>`,
-  styleUrls: [ './progress-indicator.component.css' ],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  </div>`,
+  styleUrls: ['./progress-indicator.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProgressIndicatorComponent implements OnChanges{
-
-
+export class ProgressIndicatorComponent implements OnChanges {
   @Input() progress: number = 0;
   conicGradient: string = '';
 
+  constructor(
+    private renderer: Renderer2,
+    private el: ElementRef
+  ) {}
+
   ngOnChanges(changes: SimpleChanges) {
     if (changes['progress']) {
-      this.updateConicGradient();
+      // this.updateConicGradient();
+      this.animateProgress();
     }
+  }
+
+  animateProgress() {
+    const progressCircle = this.el.nativeElement.querySelector('.progress-circle');
+    this.renderer.removeClass(progressCircle, 'animate__fadeIn');
+    this.renderer.addClass(progressCircle, 'animate__fadeIn');
+    setTimeout(() => {
+      this.updateConicGradient();
+      this.renderer.removeClass(progressCircle, 'animate__fadeOut');
+      this.renderer.addClass(progressCircle, 'animate__fadeIn');
+    }, 500); // Duración de la animación fadeOut
   }
 
   updateConicGradient() {
     const progressDegree = (this.progress / 100) * 360;
     this.conicGradient = `conic-gradient(green ${progressDegree}deg, lightgray ${progressDegree}deg)`;
-    const progressCircle = document.querySelector('.progress-circle') as HTMLElement;
-    if (progressCircle) {
-      progressCircle.style.background = this.conicGradient;
-    }
+    const progressCircle = this.el.nativeElement.querySelector('.progress-circle');
+    progressCircle.style.background = this.conicGradient;
   }
-
- }
+}
