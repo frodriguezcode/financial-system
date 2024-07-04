97c7a50e4e430981eb03dbac75c6f95a45708615
juansanchez1990 <51333698+juansanchez1990@users.noreply.github.com>
2024-07-03 20:49:56 -0600

    comit

--- src/app/demo/authentication/sign-in/sign-in.component.ts
+++ src/app/demo/authentication/sign-in/sign-in.component.ts
@@ -6,6 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormControl } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import Swal from 'sweetalert2'
+import { ConfigurationService } from 'src/app/services/configuration.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -15,9 +16,10 @@ import Swal from 'sweetalert2'
   styleUrls: ['./sign-in.component.scss']
 })
 export default class SignInComponent {
-  constructor(private autS:AuthService,    private readonly router: Router){}
+  constructor(private autS:AuthService,private readonly router: Router,private conS:ConfigurationService){}
   Usuario:FormControl=new FormControl('');
   Password:FormControl=new FormControl('');
+  Empresas:any=[]
 
   login(){
     Swal.fire({
@@ -25,23 +27,27 @@ export default class SignInComponent {
      });
      Swal.showLoading();
     this.autS.obtenerUsuarioLogin(this.Usuario.value,this.Password.value).subscribe((resp:any)=>{
-      if(resp.length>0){     
-        localStorage.setItem('usuarioFinancialSystems', JSON.stringify(resp[0]));
-        if(resp[0].ConfigInicialCompletado==false){
-          this.router.navigate(['/configuracion-inicial'])
-          Swal.close()
-        }
-        else{
-          this.router.navigate(['/analytics'])
-          Swal.fire({
-            position: "center",
-            icon: "success",
-            title: "Inicio de sesión exitosa",
-            showConfirmButton: false,
-            timer: 1500
-          });
+      localStorage.setItem('usuarioFinancialSystems', JSON.stringify(resp[0]));
 
-        }
+      if(resp.length>0){  
+         this.conS.obtenerEmpresas(resp[0].idMatriz).subscribe((resp:any)=>{
+           if(resp[0].ConfigInicialCompletado==false){
+             this.router.navigate(['/configuracion-inicial'])
+             Swal.close()
+           }
+           else{
+             this.router.navigate(['/analytics'])
+             Swal.fire({
+               position: "center",
+               icon: "success",
+               title: "Inicio de sesión exitosa",
+               showConfirmButton: false,
+               timer: 1500
+             });
+   
+           }
+
+         })   
       }
       else{
         Swal.fire({
